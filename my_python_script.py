41798c0aa31495073fa39c8eb1dcab7f2e8cd5fe
tk-riman <114357948+tk-riman@users.noreply.github.com>
2022-10-13 11:00:13 +0300

    Update my_python_script.py

--- my_python_script.py
+++ my_python_script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[30]:
+# In[1]:
 
 
 import pandas as pd
@@ -10,45 +10,44 @@ import plotly.express as px
 import streamlit as st
 
 
-# In[31]:
+# In[2]:
 
 
 height_v_country = pd.read_csv("Height of Male and Female by Country 2022.csv")
 tipping_dataset = pd.read_csv("tips.csv")
 global_plastic_prod = pd.read_csv("global-plastics-production.csv")
 
-
-# In[32]:
+# In[3]:
 
 
 fig_1 = px.box(height_v_country, x="Male Height in Cm", title="Male Height in Cm")
 
 
-# In[34]:
+# In[4]:
 
 
 fig_2 = px.violin(height_v_country, y="Female Height in Cm", title="Female Height in Cm")
 
 
-# In[36]:
+# In[5]:
 
 
 fig_3 = px.histogram(height_v_country, x="Country Name", y="Male Height in Cm", title="Average Height Vs. Country")
 
 
-# In[38]:
+# In[6]:
 
 
 fig_4 = px.scatter(tipping_dataset, x="total_bill", y="tip", color="sex", size="size", title="Tip V. Total Bill")
 
 
-# In[40]:
+# In[7]:
 
 
 fig_5 = px.line(global_plastic_prod, x="Year", y="Global plastics production (million tonnes)", title="Global Plastic Generation V. Years")
 
 
-# In[42]:
+# In[8]:
 
 
 interactive_plot_1 = st.container()
@@ -56,18 +55,19 @@ interactive_plot_2 = st.container()
 interactive_plot_3 = st.container()
 interactive_plot_4 = st.container()
 interactive_plot_5 = st.container()
+interactive_plot_6 = st.container()
 
 
-# In[46]:
+# In[14]:
 
 
 with interactive_plot_1:
-    st.header("HavE you ever wondered whether your height is average or not?")
+    st.header("Have you ever wondered whether your height is average or not?")
     st.text("Check this out, Let's start with the boys!")
     st.plotly_chart(fig_1, use_container_width=True)
 
 
-# In[47]:
+# In[15]:
 
 
 with interactive_plot_2:
@@ -75,7 +75,7 @@ with interactive_plot_2:
     st.plotly_chart(fig_2, use_container_width=True)
 
 
-# In[67]:
+# In[16]:
 
 
 with interactive_plot_3:
@@ -85,7 +85,7 @@ with interactive_plot_3:
     st.dataframe(height_v_country)
 
 
-# In[68]:
+# In[17]:
 
 
 with interactive_plot_5:
@@ -100,23 +100,45 @@ with interactive_plot_5:
     
 
 
-# In[65]:
+# In[25]:
 
 
 with interactive_plot_4:
-    st.header("Do tables with higher bills also tip higher?xd")
-    option = st.multiselect('Select at least one account please', tipping_dataset['select_gender'].drop_duplicates())
-    st.write('Your gender selected is:', option)
-#option_2 = st.sidebar.multiselect('Now, select at least one bank', df['BANCO'].drop_duplicates())
-#st.write('Your bank selected is:', option_2)  
-    tipping_dataset = tipping_dataset[tipping_dataset['select_gender'].isin(option)]
-    #df = df[df['BANCO'].isin(option_2)]
-    fig_4 = px.scatter(tipping_dataset, x="total_bill", y="tip", color="sex", size="size", title="Tip V. Total Bill")
-    st.plotly_chart(fig_4, use_container_width=True)
-    yes_tip = st.checkbox("Yes!")
-    no_tip = st.checkbox("No")
-    if yes_tip:
-        st.write("Great, as everyone should!")
-    if no_tip:
-        st.write("That's a shame, try it next time, and see the relieve in their eyes")
+    
+    sex_list = ['All'] + tipping_dataset['sex'].unique().tolist()
+    st.header("Do tables with higher bills also tip higher?")
+    st.write('You can choose the sex from box')
+    s_selected = st.selectbox('Which sex do you want to choose for the filter?', sex_list, key='start_station')
+
+    # display the collected input
+    st.write('You selected : ' + str(s_selected))
+
+    # you can filter the data based on user input and display the results in a plot
+    if s_selected != 'All':
+        display_data = tipping_dataset[tipping_dataset['sex'] == s_selected]
+        
+
+    else:
+        display_data = tipping_dataset.copy()
+        
+   
+    fig_8 = px.scatter(display_data, x="total_bill", y="tip", size="size", title="Tip V. Total Bill")
+
+    st.plotly_chart(fig_8, use_container_width=True) 
+    
+    
+with interactive_plot_6:
+    st.header("How tips differ according to size?")
+    st.text("You can choose the size from the slider below ")
+    size = st.slider('Size',1, 6)
+    slider_data = tipping_dataset[tipping_dataset['size'] == size]
+    fig_6 = px.histogram(slider_data, x="sex", y="total_bill",color="sex", title="sex vs total bill")
+
+    st.plotly_chart(fig_6, use_container_width=True)
+
+
+# In[ ]:
+
+
+
 
